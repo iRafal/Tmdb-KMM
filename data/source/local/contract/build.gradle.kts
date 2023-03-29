f0d99fa8ee72623fb9257b853866db7d2c462aa8
andrii_medvid <andrii.medvid@innovecs.com>
2023-03-30 00:26:47 +0300

    Added local data source contract

---
+++ data/source/local/contract/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    kotlin(Plugins.Kotlin.multiplatform)
+    id(Plugins.Android.library)
+}
+
+kotlin {
+    android()
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "data.source.local.contract"
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                api(project(":data:model"))
+                implementation(libs.kotlin.coroutines.core)
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val androidMain by getting
+        val androidUnitTest by getting
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+        }
+        val iosX64Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
+        val iosTest by creating {
+            dependsOn(commonTest)
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
+        }
+    }
+}
+
+android {
+    namespace = "${Versions.Android.BuildConfig.applicationId}.data.source.local.contract"
+    compileSdk = Versions.Android.BuildConfig.compileSdk
+    defaultConfig {
+        minSdk = Versions.Android.BuildConfig.minSdk
+    }
+}
